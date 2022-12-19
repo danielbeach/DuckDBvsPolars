5180f84dac5b994efcce70fb40c3a90e7de625a5
Javier A. Pérez Garza <53668813+ja-pg@users.noreply.github.com>
2022-12-18 22:09:15 -0600

    Fixes file reading error with read_csv and columns
    Fixes data loading errors by using read_csv_auto instead of read_csv with partial columns parameters.

--- duck_main.py
+++ duck_main.py
@@ -5,14 +5,7 @@ t1 = datetime.now()
 conn = duckdb.connect()
 conn.execute("SET experimental_parallel_csv=TRUE")
 conn.execute("""SELECT date, SUM(failure) as failures
-                FROM read_csv('data/*/*.csv', delim=',', header=True,  IGNORE_ERRORS=1, 
-                    columns={
-                            'date': 'DATE',
-                            'serial_number' : 'VARCHAR',
-                            'model' : 'VARCHAR',
-                            'capacity_bytes' : 'VARCHAR',
-                            'failure' : 'INT'
-                            })
+                FROM read_csv_auto('data/*/*.csv', delim=',', header=True,  IGNORE_ERRORS=1)
              GROUP BY date;""")
 print(conn.fetchall())
 t2 = datetime.now()
